5aa185fa8f88d5df981f856f9b8f65b08b8eb0c0
bhp0122 <patel.b0122@gmail.com>
2023-12-01 18:05:28 -0600

    Adjusted filter position

--- src/components/layoutHandlers/SidebySide.js
+++ src/components/layoutHandlers/SidebySide.js
@@ -268,7 +268,7 @@ function SidebySide(props) {
     return (
         <div>
             {/* Dropdown Menu for the user to filter by Courses and TAs */}
-            <div class="w3-sidebar w3-border w3-bar-block" style={{width: "10%", height: "auto", position: "relative", float: "left", left: 10, bottom:"38%", padding: "5px"}}>
+            <div class="w3-sidebar w3-border w3-bar-block" style={{width: "10%", height: "auto", position: "relative", float: "left", left: 10, bottom:"35%", padding: "5px"}}>
                     {/* OnChange defines the function/variable/method that gets triggered whenever an option is picked by the user. */}
                     <select id="chooseCourse" onChange={handleCourseFilter}>
                         <option value="None">Course</option>
